f396cfaf3054a97a80321b8d788e96ae2d290658
dalebar <155585623+dalebar@users.noreply.github.com>
2025-02-20 10:44:47 +0000

    Add copied files

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
-# python-practise
+# Data Engineering Pre-Course Exercises
+
+## Purpose
+
+These exercises are intended to give you some experience of writing simple Python code.
+
+## Prerequisites
+
+You need to _fork and clone_ this repository. You should already have watched
+[this introduction](https://www.youtube.com/watch?v=4VjoJyqQkNQ&ab_channel=Tutor1) to forking and cloning.
+When you have cloned the repository to the local machine, then open it with Visual Studio Code
+(or the IDE of your choice).
+
+Before you start, you should have the latest version of Python 3 installed. If you have not already
+done so, follow the steps described in `Preparing For The Course` in the Pre-Course notes.
+
+To test if Python is installed correctly, type at the terminal command prompt:
+
+```bash
+python --version
+```
+
+You should see something like:
+
+```
+Python 3.11.5
+```
+
+(The digits will be different depending on what exact version you installed. As long as the
+version number is higher than 3.9.0, everything should be fine.)
+If you don't have Python installed then go back to the `Preparing For The Course` section and
+repeat the steps there. Failing that, get in touch with us in your pre-course slack channel.
+
+## Running tests
+
+You will be asked to write some code in the exercise. To test whether you have got it right,
+we will run some automated tests using some tests that we've pre-written for you. You will be learning a _lot_ more
+about testing in general and also about a tool called `pytest` during the course. For now, please just run the files with python like so:
+
+```bash
+python PATH_TO_FILE.py
+```
+
+at the command prompt.
+
+## Completing and testing the exercises.
+
+There are two types of exercise included here.
+
+### REPL Exercises
+
+The exercises in Section `01_variables` are intended to be executed in the Python REPL.
+
+The Python _REPL_ (**R**ead **E**valuate **P**rint **L**oop) is a command line tool for running Python code interactively one instruction at a time.
+To start the REPL, just type
+
+```bash
+python
+```
+
+at the command prompt.
+
+You should see a new Python prompt, like this:
+
+![Python Prompt](./img/python_prompt.png)
+
+Now, you can type Python code instructions, followed by the `Return` key.
+
+The exercises invite you to type in various commands,
+predicting what they might do in advance. For example, what might this command do?
+
+```
+>>> print('Hello New Northcoder!')
+```
+
+To terminate the REPL session, type `exit()` or hit `Ctrl+d`
+
+### Function Exercises
+
+The remaining exercises all require you to write (or rewrite) some Python _functions_. Make sure you have read the pre-course
+notes on functions first.
+
+To test how you coded these functions we will you the pre-written tests.
+
+For example, the file `00_demonstration.py` file has a very simple function that is going wrong. You
+should be able to test this by typing (at the command prompt):
+
+```bash
+python 00_demonstration.py
+```
+
+You should see a message saying that one of the tests is passing and one is being skipped. Now, in VS Code, navigate to the `00_demonstration.py` file.
+
+Change the `SkipCheck` on line 30 to `Check`.
+
+You should now be able to re-run `python 00_demonstration.py` and see the second test is failing.
+
+Now change the code to add 2 to the counter and re-run the `python` command to see the test passing. Congratulations!
+
+In most cases, you will be asked to write or change functions to illustrate different points about Python code. Please do not change the tests themselves, which are written below the function being tested.
+
+Happy Coding!
---
+++ test_api/checks.py
@@ -0,0 +1,135 @@
+import copy
+
+# ANSI colour codes
+BOLD_RED = "\033[1;31m"
+NORMAL_RED = "\033[0;31m"
+BOLD_GREEN = "\033[1;32m"
+NORMAL_GREEN = "\033[0;32m"
+BOLD_YELLOW = "\033[1;33m"
+NORMAL_YELLOW = "\033[0;33m"
+DEFAULT = "\033[0;37m"
+
+
+class SkipCheck:
+    def __init__(self, func, title):
+        self.func = func
+        self.title = title
+
+    def when_called_with(self, *args):
+        return self
+
+    def is_same_as(self, original_input):
+        print(
+            f"{BOLD_YELLOW}{self.func.__name__}(){NORMAL_YELLOW}, Test {self.title}: skipping test...{DEFAULT}"  # noqa
+        )
+
+    def is_not_same_as(self, original_data):
+        print(
+            f"{BOLD_YELLOW}{self.func.__name__}(){NORMAL_YELLOW}, Test {self.title}: skipping test...{DEFAULT}"  # noqa
+        )
+
+    def mutates_input(self, label):
+        print(
+            f"{BOLD_YELLOW}{self.func.__name__}(){NORMAL_YELLOW}, Test {self.title}: skipping test...{DEFAULT}"  # noqa
+        )
+
+    def does_not_mutate_input(self, label):
+        print(
+            f"{BOLD_YELLOW}{self.func.__name__}(){NORMAL_YELLOW}, Test {self.title}: skipping test...{DEFAULT}"  # noqa
+        )
+
+    def is_type(self, data_type):
+        print(
+            f"{BOLD_YELLOW}{self.func.__name__}(){NORMAL_YELLOW}, Test {self.title}: skipping test...{DEFAULT}"  # noqa
+        )
+
+    def returns(self, return_value):
+        print(
+            f"{BOLD_YELLOW}{self.func.__name__}(){NORMAL_YELLOW}, Test {self.title}: skipping test...{DEFAULT}"  # noqa
+        )
+
+
+class Check:
+    def __init__(self, func, title):
+        self.func = func
+        self.title = title
+
+    def when_called_with(self, *args):
+        self.args = args
+        self.args_copy = copy.deepcopy(args)
+        return self
+
+    def is_same_as(self, input_data):
+        self._set_return_value()
+
+        if self.return_value is input_data:
+            feedback_msg = f"{BOLD_GREEN}{self.func.__name__}(){NORMAL_GREEN}, Test {self.title}: Test passed, same object returned{DEFAULT}"  # noqa
+
+        else:
+            feedback_msg = f"{BOLD_RED}{self.func.__name__}(){NORMAL_RED}, Test {self.title}: Test failed, return value should be the same object{DEFAULT}"  # noqa
+
+        print(feedback_msg)
+
+    def is_not_same_as(self, original_data):
+        self._set_return_value()
+
+        if self.return_value is not original_data:
+            feedback_msg = f"{BOLD_GREEN}{self.func.__name__}(){NORMAL_GREEN}, Test {self.title}: Test passed, new Python object returned{DEFAULT}"  # noqa
+
+        else:
+            feedback_msg = f"{BOLD_RED}{self.func.__name__}(){NORMAL_RED}, Test {self.title}: Test failed, return value should be a new {type(original_data).__name__}{DEFAULT}"  # noqa
+
+        print(feedback_msg)
+
+    def mutates_input(self, label):
+        self._set_return_value()
+
+        if self.args != self.args_copy:
+            feedback_msg = f"{BOLD_GREEN}{self.func.__name__}(){NORMAL_GREEN}, Test {self.title}: Test passed, {label} successfully mutated{DEFAULT}"  # noqa
+        else:
+            feedback_msg = f"{BOLD_RED}{self.func.__name__}(){NORMAL_RED}, Test {self.title}: Test failed, {label} has not been mutated{DEFAULT}"  # noqa
+
+        print(feedback_msg)
+
+    def does_not_mutate_input(self, label):
+        self._set_return_value()
+
+        if self.args == self.args_copy:
+            feedback_msg = f"{BOLD_GREEN}{self.func.__name__}(){NORMAL_GREEN}, Test {self.title}: Test passed, {label} not mutated{DEFAULT}"  # noqa
+        else:
+            feedback_msg = f"{BOLD_RED}{self.func.__name__}(){NORMAL_RED}, Test {self.title}: Test failed, {label} should not be mutated{DEFAULT}"  # noqa
+
+        print(feedback_msg)
+
+    def is_type(self, data_type):
+        self._set_return_value()
+
+        if isinstance(self.return_value, data_type):
+            feedback_msg = f"{BOLD_GREEN}{self.func.__name__}(){NORMAL_GREEN}, Test {self.title}: Test passed, correct data type returned{DEFAULT}"  # noqa
+        else:
+            feedback_msg = f"{BOLD_RED}{self.func.__name__}(){NORMAL_RED}, Test {self.title}: Return value should be of type {self.return_value.__class__.__name__}{DEFAULT}"  # noqa
+
+        print(feedback_msg)
+
+    def _set_return_value(self):
+        try:
+            if hasattr(self, "args"):
+                self.return_value = self.func(*self.args)
+            else:
+                self.return_value = self.func()
+        except Exception:
+            feedback_msg = (f"{BOLD_RED}{self.func.__name__}(){NORMAL_RED}, "
+                            f"Test {self.title}: Test failed, see error "
+                            f"message below{DEFAULT}\n")
+            print(feedback_msg)
+            raise
+
+    def returns(self, expected_return_value):
+        self._set_return_value()
+
+        if self.return_value == expected_return_value:
+            feedback_msg = f"{BOLD_GREEN}{self.func.__name__}(){NORMAL_GREEN}, Test {self.title}: Test passed{DEFAULT}"  # noqa
+        else:
+            feedback_msg = f"{BOLD_RED}{self.func.__name__}(){NORMAL_RED}, Test {self.title}: expected '{expected_return_value}', but received '{self.return_value}'{DEFAULT}"  # noqa
+
+        print(feedback_msg)
